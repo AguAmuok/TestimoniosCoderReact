633ab56dd09befd35c0603ba74187980b2ad0c18
Agustin Amuchástegui <aguamu24@gmail.com>
2022-06-04 19:21:34 -0300

    guardado segundo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import Testimonio from './components/Testimonio';
 
 function App() {
   return (
     <div className="App">
+
+      <div className='contenedor-principal'><h1>ACA ESTA LA DESCRIPCION DE TODOS</h1>
       <Testimonio />
+      </div>
     </div>
+
   );
 }
 
--- src/components/testimonio.js
+++ src/components/testimonio.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import '../stylesheets/testimonio.css'
 
-function testmonio() {
+function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio'
-        src={require('../imagenes/agu.png')}
+        src={require('../imagenes/agu.JPG')}
         alt='imagen de agu'
         />
         <div className='contenedor-texto-testimonio'>
           <p className='nombre-testimonio'>Agustin en la Falda</p>
           <p className='cargo-testimonio'>Futuro Developer</p>
-          <p className='texto-testimonio'>Soy Agustin un futuro developer Full Stack Mern</p> 
+          <p className='texto-testimonio'>Soy Agustin un futuro developer Full Stack Mern
+          Tengo una Honda XR 250 me gustan las milanesas, pizzas, asados y lomitos.</p> 
         </div>
     </div> 
 
   );
 }
+
+export default Testimonio;
     
